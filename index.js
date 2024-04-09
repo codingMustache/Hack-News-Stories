f0b6cc4c4ec903136aa7b66b5c9a4ce5478f617c
Jorge <jorge@codingMustache.dev>
2024-04-09 15:15:16 -0500

    added some color

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-
 const fetchTopStories = async () => {
   try {
-    const req = await fetch(
-      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
-    );
+    const req = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
 
     const json = await req.json();
     const topTen = json.slice(0, 20);
 
     const topTenStoriesReq = await Promise.all(
       topTen.map(async (id) =>
-        fetch(
-          `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`,
-        ),
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`),
       ),
     );
 
-    const topTenStoriesJson = await Promise.all(
-      topTenStoriesReq.map((req) => req.json()),
-    )
+    const topTenStoriesJson = await Promise.all(topTenStoriesReq.map((req) => req.json()));
 
-    const topTenStories = topTenStoriesJson.map((story) => {
-      console.log(`${story.title} \n ${story.url} \n\n`)
-      
-        return {
-        title: story.title,
-        url: story.url,
-      }
-    })
-    
+    topTenStoriesJson.map((story) => {
+      console.log("\x1b[31m", `${story.title}`);
+      console.log("\x1b[34m", `${story.url} \n`);
+    });
+
     //console.log(topTenStories);
   } catch {
     console.log("error");
