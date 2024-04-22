6527f4928f790b47dbeea2573ccaa418cc7d5c74
Jorge <jorge@codingMustache.dev>
2024-04-22 11:55:11 -0500

    using a class for better readablity

--- index.js
+++ index.js
@@ -1,55 +1,64 @@
 import { emitKeypressEvents } from "readline";
 import { exec } from "child_process";
 
-const fetchTopStories = async () => {
-  try {
-    // Retrieving top news stories IDs
-    const response = await fetch(
-      "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
-    );
-    const storyIds = await response.json();
+class HackerNewsReader {
+  constructor() {
+    this.pageSize = 10;
+    this.currentPage = 1;
+    this.currentStories = [];
+    this.storyIds = [];
+  }
 
-    const pageSize = 10;
-    let currentPage = 1;
-    let currentStories = [];
-
-    const paginate = async (pageNumber) => {
-      const start = (pageNumber - 1) * pageSize;
-      const end = pageNumber * pageSize;
-      const pageItems = storyIds.slice(start, end);
-
-      const storyDetails = await Promise.all(
-        pageItems.map((id) =>
-          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`).then((res) =>
-            res.json(),
-          ),
-        ),
+  async fetchTopStoriesIds() {
+    try {
+      const response = await fetch(
+        "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
       );
+      this.storyIds = await response.json();
+    } catch (error) {
+      console.error("Failed to fetch top stories:", error);
+    }
+  }
 
-      return storyDetails.map(({ title, url }) => ({ title, url }));
-    };
+  async paginate(pageNumber) {
+    const start = (pageNumber - 1) * this.pageSize;
+    const end = pageNumber * this.pageSize;
+    const pageItems = this.storyIds.slice(start, end);
 
-    const displayPage = async (pageNumber) => {
-      currentStories = await paginate(pageNumber);
-      console.clear();
-      currentStories.forEach((story, index) => {
-        console.log(`\x1b[34m${index}: \x1b[0m${story.title}`);
-      });
-      console.log("-".repeat(process.stdout.columns));
-    };
+    const storyDetails = await Promise.all(
+      pageItems.map((id) =>
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`).then((res) =>
+          res.json(),
+        ),
+      ),
+    );
 
-    await displayPage(currentPage);
+    return storyDetails.map(({ title, url }) => ({ title, url }));
+  }
+
+  async displayPage(pageNumber) {
+    this.currentStories = await this.paginate(pageNumber);
+    console.clear();
+    this.currentStories.forEach((story, index) => {
+      console.log(`\x1b[34m${index}:  \x1b[31m${story.title}`);
+    });
+    console.log("\x1b[32m", "-".repeat(process.stdout.columns - 2));
+  }
+
+  async startReading() {
+    await this.fetchTopStoriesIds();
+    await this.displayPage(this.currentPage);
 
     emitKeypressEvents(process.stdin);
     process.stdin.setRawMode && process.stdin.setRawMode(true);
 
     process.stdin.on("keypress", async (str, key) => {
       if (str === ".") {
-        currentPage++;
-        await displayPage(currentPage);
-      } else if (key.name.match(/[0-9]/) && parseInt(key.name) <= currentStories.length) {
+        this.currentPage++;
+        await this.displayPage(this.currentPage);
+      } else if (key.name.match(/[0-9]/) && parseInt(key.name) <= this.currentStories.length) {
         const storyIndex = parseInt(key.name);
-        exec(`open ${currentStories[storyIndex].url}`, (err) => {
+        exec(`open ${this.currentStories[storyIndex].url}`, (err) => {
           if (err) {
             console.error("Failed to open URL:", err);
           }
@@ -59,9 +68,8 @@ const fetchTopStories = async () => {
         process.exit();
       }
     });
-  } catch (error) {
-    console.error("Failed to fetch top stories:", error);
   }
-};
+}
 
-fetchTopStories();
+const hackerNewsReader = new HackerNewsReader();
+hackerNewsReader.startReading();
