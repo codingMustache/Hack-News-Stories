0f8183c21ae47587b482e28078bcd0ff22c152fc
Jorge <jorge@codingMustache.dev>
2024-04-23 09:20:25 -0500

    trying to get as small as possible

--- README.md
+++ README.md
@@ -17,19 +17,7 @@ hn-clireader -RunMode
 | -r -read  | Runs in read mode  |
 | -q -quiet | Runs in quiet mode |
 
-Pagination is done with `.`, using the number, `0-9`, to the right of the story title you can open the story in your default browser. You can exit the program using any key except `0-9` and `.`
-
-Run example:
-
-```
-0:  Meta Horizon OS
-1:  Equinox.space
-2:  An Exploration of SBCL Internals (2020)
-3:  Parquet-WASM: Rust-based WebAssembly bindings to read and write Parquet data
-4:  Show HN: OpenOrb, a curated search engine for Atom and RSS feeds
-5:  HockeyStack (YC S23) Is Hiring Young Founding Engineers in the Bay Area
-6:  Py2wasm – A Python to WASM Compiler
-7:  Inside the Super Nintendo cartridges
-8:  JEDEC Extends DDR5 Memory Spec to 8800 MT/S, Adds Anti-Rowhammer Features
-9:  Intel Gaudi 3 the New 128GB HBM2e AI Chip in the Wild
-```
+| cmd | Mode information |
+| --- | ---------------- |
+| .   | Paginate         |
+| 0-1 | Open link        |
--- index.js
+++ index.js
@@ -6,36 +6,36 @@ class HackerNewsReader {
   constructor() {
     this.pageSize = 10;
     this.currentPage = 1;
-    this.currentStories = [];
     this.storyIds = [];
-    this.runMode = "";
+    this.runModes = [];
+    this.currentStories = [];
   }
 
   async fetchTopStoriesIds() {
     try {
-      const response = await fetch(
-        "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty",
-      );
-      this.storyIds = await response.json();
+      this.storyIds = await (
+        await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+      ).json();
     } catch (error) {
       console.error("Failed to fetch top stories:", error);
     }
   }
 
   async paginate(pageNumber) {
-    const start = (pageNumber - 1) * this.pageSize;
-    const end = pageNumber * this.pageSize;
-    const pageItems = this.storyIds.slice(start, end);
-
-    const storyDetails = await Promise.all(
-      pageItems.map((id) =>
-        fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`).then((res) =>
-          res.json(),
-        ),
-      ),
+    const pageItems = this.storyIds.slice(
+      (pageNumber - 1) * this.pageSize,
+      pageNumber * this.pageSize,
     );
 
-    return storyDetails.map(({ title, url }) => ({ title, url }));
+    return (
+      await Promise.all(
+        pageItems.map((id) =>
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`).then((res) =>
+            res.json(),
+          ),
+        ),
+      )
+    ).map(({ title, url }) => ({ title, url }));
   }
 
   async displayPage(pageNumber) {
@@ -47,22 +47,8 @@ class HackerNewsReader {
     });
   }
 
-  getOpenCommand(url) {
-    const commands = {
-      darwin: "open",
-      win32: "start",
-      linux: "xdg-open",
-    };
-
-    const command = commands[process.platform];
-    if (!command) {
-      throw new Error(`Unsupported platform: ${process.platform}`);
-    }
-
-    return `${command} "${url}"`;
-  }
-
-  async startReading() {
+  async startReading(runModes) {
+    this.runModes = runModes;
     await this.fetchTopStoriesIds();
     await this.displayPage(this.currentPage);
 
@@ -73,13 +59,16 @@ class HackerNewsReader {
       if (str === ".") {
         this.currentPage++;
         await this.displayPage(this.currentPage);
-      } else if (key.name.match(/[0-9]/) && parseInt(key.name) <= this.currentStories.length) {
-        const storyIndex = parseInt(key.name);
-        exec(this.getOpenCommand(this.currentStories[storyIndex].url), (e) => {
-          if (e) {
-            console.error("Failed to open URL:", err);
-          }
-        });
+      } else if (str.match(/[0-9]/)) {
+        const commands = { darwin: "open", win32: "start", linux: "xdg-open" };
+        exec(
+          `${
+            commands[process.platform] ||
+            (() => {
+              throw new Error(`Unsupported platform: ${process.platform}`);
+            })()
+          } "${this.currentStories[parseInt(key.name)].url}"`,
+        );
       } else {
         console.log("Exiting");
         process.exit();
@@ -88,32 +77,20 @@ class HackerNewsReader {
   }
 
   printRun() {
-    const welcomeStr = "\x1b[1m\x1b[4\x1b[4mHacker News CLI Reader\x1b[0m\n";
+    const welcomeStr = "\x1b[0m\x1b[1m\x1b[4\x1b[4mHacker News CLI Reader\x1b[0m\n";
     const makeAlias =
       'Make this program an alias: \n\n unix/linix:\n\x1b[1m  alias hn="hn-cli-reader -q" \n Powershell:\n\x1b[1m Set-Alias -Name hn -Value hn-cli-reader \x1b[0m\n\n';
     const runCmds =
-      "\x1b[0mUse the --read -r flag to start reading Hacker News stories\n Use the --quite -q flag to run program in quiet mode\n";
+      "\x1b[0mUse the --read -r flag to start reading Hacker News stories\n Use the --quiet -q flag to run program in quiet mode\n";
     const instructions = "Use . to paginate\n Use 0-9 to open link in browser\n";
-    if (this.runMode === "quiet") {
-      return;
-    } else if (this.runMode === "read") {
-      console.log(welcomeStr, instructions);
-    } else {
-      console.log(welcomeStr, makeAlias, runCmds);
-      process.exit();
-    }
-  }
-  setPrintMode(passedArgs) {
-    if (passedArgs.some((arg) => /-q(?:uiet)?/.test(arg))) {
-      this.runMode = "quiet";
-    } else if (passedArgs.some((arg) => /-r(?:ead)?/.test(arg))) {
-      this.runMode = "read";
-    } else {
-      this.runMode = "verbose";
-    }
+
+    this.runModes.some((arg) => /-q(?:uiet)?/.test(arg))
+      ? ""
+      : this.runModes.some((arg) => /-r(?:ead)?/.test(arg))
+        ? console.log(welcomeStr, instructions)
+        : console.log(welcomeStr, makeAlias, runCmds);
   }
 }
+
 const pgm = new HackerNewsReader();
-const args = process.argv.slice(2);
-pgm.setPrintMode(args);
-pgm.startReading();
+pgm.startReading(process.argv.slice(2));
